3b908e4b54aae5579a5e525d67a7e34aa764f1d1
apriyanka02 <apriyanka2002@gmail.com>
2026-05-25 18:41:58 +0530

    Added JDBC railway reservation logic

---
+++ src/Repo/DBConnection.java
@@ -0,0 +1,40 @@
+package Repo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/railway_reservation";
+
+    private static final String USERNAME =
+            "root";
+
+    private static final String PASSWORD =
+            "admin@123";
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+            con = DriverManager.getConnection(
+                    URL,
+                    USERNAME,
+                    PASSWORD
+            );
+
+            System.out.println(
+                    "Database Connected Successfully"
+            );
+
+        }
+        catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
